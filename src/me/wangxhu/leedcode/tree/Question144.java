f9f2993f057e2d6978b3d485efd56d32daf7a0f4
StormWangxhu <StormWangxhu@163.com>
2018-12-01 02:04:06 +0800

    leetcode144 求二叉树的前序遍历非递归实现数据结构:栈

--- src/me/wangxhu/leedcode/tree/Question144.java
+++ src/me/wangxhu/leedcode/tree/Question144.java
@@ -4,6 +4,7 @@ import me.wangxhu.leedcode.dfs.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <p>Created on 18-12-1</p>
@@ -14,6 +15,8 @@ import java.util.List;
  * leedcode 144 :求二叉树的前序遍历
  * 难度系数:中等
  * 递归和非递归实现
+ *
+ * 前序遍历: 根 左 右
  */
 public class Question144 {
 
@@ -23,6 +26,7 @@ public class Question144 {
     static class Solution {
 
         static List<Integer> res;
+
         public List<Integer> preorderTraversal(TreeNode root) {
             res = new ArrayList<>();
             preOrderTraversal(root);
@@ -30,13 +34,47 @@ public class Question144 {
         }
 
         private void preOrderTraversal(TreeNode root) {
-            if (root==null){
+            if (root == null) {
                 return;
             }
 
             res.add(root.val);
             preOrderTraversal(root.left);
             preOrderTraversal(root.right);
+        }
+    }
+
+
+    /**
+     * 非递归实现  数据结构:栈
+     */
+    static class Solution2 {
+        static List<Integer> res;
+
+        public List<Integer> preorderTraversal(TreeNode root) {
+            res = new ArrayList<>();
+            preOrderTravsersal(root);
+            return res;
+        }
+
+        private void preOrderTravsersal(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                root = stack.pop();
+                res.add(root.val);
+                if (root.right != null) {
+                    stack.push(root.right);
+                }
+                if (root.left != null) {
+                    stack.push(root.left);
+                }
+            }
+            System.out.println();
         }
     }
 }
