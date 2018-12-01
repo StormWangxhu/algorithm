378a94b08fef12b2a30899003e0f347a4379e588
StormWangxhu <StormWangxhu@163.com>
2018-12-01 14:20:27 +0800

    leetcode226 翻转二叉树 递归

---
+++ src/me/wangxhu/leedcode/tree/Question226.java
@@ -0,0 +1,32 @@
+package me.wangxhu.leedcode.tree;
+
+import me.wangxhu.leedcode.dfs.TreeNode;
+
+/**
+ * <p>Created on 18-12-1</p>
+ *
+ * @author:StormWangxhu
+ * @description: <p>描述</p>
+ * <p>
+ * leetcode226 :翻转二叉树
+ *
+ * 难度级别:简单
+ *
+ * 翻转二叉树:即以根节点为对称轴,左右节点交换值
+ */
+public class Question226 {
+
+    static class Solution {
+
+        public TreeNode invertTree(TreeNode root) {
+            if (root==null){
+                return null;
+            }
+
+            TreeNode left = root.left;
+            root.left = invertTree(root.right);
+            root.right =invertTree(left);
+            return root;
+        }
+    }
+}
