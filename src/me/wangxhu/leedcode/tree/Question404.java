046e7fe3366bf51d67f99dbf9dd0e23960b6e919
StormWangxhu <StormWangxhu@163.com>
2018-12-01 16:32:51 +0800

    leetcode404 求左叶子之和 递归求解

---
+++ src/me/wangxhu/leedcode/tree/Question404.java
@@ -0,0 +1,37 @@
+package me.wangxhu.leedcode.tree;
+
+import me.wangxhu.leedcode.dfs.TreeNode;
+
+/**
+ * <p>Created on 18-12-1</p>
+ *
+ * @author:StormWangxhu
+ * @description: <p>描述</p>
+ * <p>
+ * leetcode404 : 求左子树叶子节点的和/左叶子之和
+ * <p>
+ * 难度级别:简单
+ */
+public class Question404 {
+
+    static class Solution {
+
+        public int sumOfLeftLeaves(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            if (isLeaf(root.left)) {
+                return root.left.val + sumOfLeftLeaves(root.right);
+            }
+
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+
+        private boolean isLeaf(TreeNode root) {
+            if (root == null) {
+                return false;
+            }
+            return root.left == null && root.right == null;
+        }
+    }
+}
