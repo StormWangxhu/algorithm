d2a1a0717e001d7dbed564c1d63b7a626bb5dea0
StormWangxhu <StormWangxhu@163.com>
2018-12-02 14:41:45 +0800

    leetcode538 将二叉搜索树转换为累加树 递归实现

--- src/me/wangxhu/leedcode/tree/BST/Question538.java
+++ src/me/wangxhu/leedcode/tree/BST/Question538.java
@@ -10,6 +10,7 @@ import me.wangxhu.leedcode.dfs.TreeNode;
  * <p>
  * <p>
  * leetcode538 : 把二叉搜索树转换为累加树
+ * 二叉搜索树: 根节点大于等于左节点,小于等于右节点
  * <p>
  * 难度级别: 简单
  */
@@ -28,9 +29,11 @@ public class Question538 {
             if (root == null) {
                 return;
             }
+            //先遍历右子树
             traver(root.right);
             sum += root.val;
             root.val = sum;
+            //再遍历左子树
             traver(root.left);
         }
     }
