a86d057a4877cb96fa599b0731681684b7cef283
StormWangxhu <StormWangxhu@163.com>
2018-12-09 22:04:10 +0800

    leetcode234 判断一个链表是否是回文结构

--- src/me/wangxhu/leedcode/linkedlist/Question234.java
+++ src/me/wangxhu/leedcode/linkedlist/Question234.java
@@ -14,6 +14,8 @@ import java.util.Stack;
  * leetcode234.判断一个链表是否是回文结构
  * <p>
  * 难度级别:简单
+ *
+ * 思路:就是一个链表要是回文链表,则借用栈结构可以实现反转,再一次遍历链表,若栈顶值和链表元素都相同,则是回文结构,否则不是回文结构!
  */
 public class Question234 {
 
@@ -42,6 +44,31 @@ public class Question234 {
                 head = head.next;
             }
 
+            return true;
+        }
+    }
+
+    /**
+     * 这个例子可以全部通过
+     */
+    static class Solution1 {
+
+        public boolean isPalindrome(ListNode head) {
+
+            Stack<ListNode> stack = new Stack<>();
+            ListNode cur = head;
+
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.next;
+            }
+
+            while (head != null) {
+                if (head.val != stack.pop().val) {
+                    return false;
+                }
+                head = head.next;
+            }
             return true;
         }
     }
