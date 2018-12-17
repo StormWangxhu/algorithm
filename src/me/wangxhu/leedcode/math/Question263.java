399af474b1e06d7b9c07afb757dfee27d5bf7140
StormWangxhu <StormWangxhu@163.com>
2018-12-17 20:11:53 +0800

    leetcode263 数学：丑数

---
+++ src/me/wangxhu/leedcode/math/Question263.java
@@ -0,0 +1,22 @@
+package me.wangxhu.leedcode.math;
+
+/**
+ * <p>Created on 18-12-17</p>
+ *
+ * @author:StormWangxhu
+ * @description: <p>描述</p>
+ *
+ *
+ * leetcode263. 丑树
+ *
+ * 难度级别：简单
+ */
+public class Question263 {
+
+    static class Solution{
+
+        public boolean isUgly(int num) {
+            return true;
+        }
+    }
+}
