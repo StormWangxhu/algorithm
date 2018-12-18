5157302a42759f6f266ae9e6498792d08df6b459
StormWangxhu <StormWangxhu@163.com>
2018-12-18 11:04:11 +0800

    leetcode704 二分查找（模板1）+模板2

--- src/me/wangxhu/leedcode/array/Question704.java
+++ src/me/wangxhu/leedcode/array/Question704.java
@@ -37,4 +37,32 @@ public class Question704 {
             return -1;
         }
     }
+
+
+    /**
+     * 二分查找模板2：高级二分查找
+     */
+    static class Solution1 {
+
+        public int search(int[] nums, int target) {
+
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int left = 0, right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            if (left != nums.length && nums[left] == target)
+                return left;
+            return -1;
+        }
+    }
 }
