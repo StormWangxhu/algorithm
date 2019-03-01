057d8611f9d225d634dfd2fa60ff73559bf0315b
StormWangxhu <StormWangxhu@163.com>
2019-03-01 09:54:32 +0800

    求解斐波那契数列，使用递归以及动态规划及其缓存

--- src/me/wangxhu/nowcoder/math/Fibonacci.java
+++ src/me/wangxhu/nowcoder/math/Fibonacci.java
@@ -77,15 +77,15 @@ public class Fibonacci {
 
         private int[] fib = new int[40];
 
-        public Solution4(){
-            fib[1]=1;
-            fib[2]=2;
-            for (int i = 2; i <fib.length ; i++) {
-                fib[i]=fib[i-1]+fib[i-2];
+        public Solution4() {
+            fib[1] = 1;
+            fib[2] = 2;
+            for (int i = 2; i < fib.length; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
             }
         }
 
-        public int Fibonacci(int n){
+        public int Fibonacci(int n) {
             return fib[n];
         }
     }
