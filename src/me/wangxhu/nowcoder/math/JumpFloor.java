21c8d122968e1dae316704238feb8d0d733bfff2
StormWangxhu <StormWangxhu@163.com>
2019-03-01 10:53:47 +0800

    跳台阶问题

--- src/me/wangxhu/nowcoder/math/JumpFloor.java
+++ src/me/wangxhu/nowcoder/math/JumpFloor.java
@@ -52,4 +52,27 @@ public class JumpFloor {
         }
     }
 
+    /**
+     * 和只与前两个数有关
+     * 17ms
+     */
+    static class Solution3 {
+
+        public int JumpFloor(int target) {
+
+            if (target <= 2) {
+                return target;
+            }
+
+            int pre1 = 1, pre2 = 2;
+            int result = 0;
+            for (int i = 3; i <= target; i++) {
+                result = pre1 + pre2;
+                pre1 = pre2;
+                pre2 = result;
+            }
+            return result;
+        }
+    }
+
 }
