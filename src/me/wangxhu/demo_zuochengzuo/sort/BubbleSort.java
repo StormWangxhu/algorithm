175ac5c0cb2172ef9d2b21be90a1904dacbfd1af
StormWangxhu <StormWangxhu@163.com>
2019-03-02 11:48:26 +0800

    冒泡排序 O（N^2）

--- src/me/wangxhu/demo_zuochengzuo/sort/BubbleSort.java
+++ src/me/wangxhu/demo_zuochengzuo/sort/BubbleSort.java
@@ -10,28 +10,26 @@ package me.wangxhu.demo_zuochengzuo.sort;
  */
 public class BubbleSort {
 
-    static class Bubble_Sort {
+    public static void bubbleSort(int[] arr) {
 
-        public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
 
-            if (arr == null || arr.length < 2) {
-                return;
-            }
-
-            for (int end = arr.length - 1; end > 0; end--) {
-                for (int i = 0; i < end; i++) {
-                    if (arr[i] > arr[i + 1]) {
-                        swap(arr, i, i + 1);
-                    }
+        for (int end = arr.length - 1; end > 0; end--) {
+            for (int i = 0; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
                 }
             }
         }
-
-
-        public static void swap(int[] arr, int i, int j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
     }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
