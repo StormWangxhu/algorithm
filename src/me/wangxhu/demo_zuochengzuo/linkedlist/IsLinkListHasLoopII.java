70dec6a5098cff495fe77ed0a98ea575579e00eb
StormWangxhu <StormWangxhu@163.com>
2019-03-05 11:32:50 +0800

    判断链表是否有环,方法二：哈希表

--- src/me/wangxhu/demo_zuochengzuo/linkedlist/IsLinkListHasLoopII.java
+++ src/me/wangxhu/demo_zuochengzuo/linkedlist/IsLinkListHasLoopII.java
@@ -1,11 +1,35 @@
 package me.wangxhu.demo_zuochengzuo.linkedlist;
 
+import me.wangxhu.demo_zuochengzuo.common.ListNode;
+
+import java.util.HashSet;
+
 /**
  * @Author: StormWangxhu
- * @Time: 2019-03-05 11:20
+ * @Time: 2019-03-05 11:25
  * @Email: StormWangxhu@163.com
- * @Description: 判断一个链表是否有环
- * 用哈希表做：
+ * @Description: 判断链表是否有环
+ * 方法二：利用哈希表来做HashSet.
+ * 将所有节点都放进哈希表中，若哈希表中第一次遇见已经存在的节点，即为第一个入环节点
+ * 若遍历完都没有遇到哈希表中已经存在的节点，则该链表没有环
  */
 public class IsLinkListHasLoopII {
+
+    public ListNode getLoopListNode(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        HashSet<ListNode> hashSet = new HashSet<>();
+        while (head != null) {
+            if (hashSet.contains(head)) {
+                return head;
+            }
+            hashSet.add(head);
+            head = head.next;
+        }
+
+        return null;
+    }
 }
