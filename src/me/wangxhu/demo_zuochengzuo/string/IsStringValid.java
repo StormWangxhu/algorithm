5fd80719c6e0f3b2dc7ec239fac3ded6bce9d300
StormWangxhu <StormWangxhu@163.com>
2019-03-21 20:16:31 +0800

    括号字符串的有效性

--- src/me/wangxhu/demo_zuochengzuo/string/IsStringValid.java
+++ src/me/wangxhu/demo_zuochengzuo/string/IsStringValid.java
@@ -5,6 +5,9 @@ package me.wangxhu.demo_zuochengzuo.string;
  * @Time: 2019-03-21 09:31
  * @Email: StormWangxhu@163.com
  * @Description: 括号字符串的有效性
+ * 算法：count值遇到左括号就++,遇到右括号就--。
+ * 如果中途没有count为负值的情况，那么当遍历完后，count值为0返回true，不为0，返回false。
+ * 因为一个有效的括号组合，左括号和右括号数量是一样多的。
  */
 public class IsStringValid {
 
@@ -15,19 +18,19 @@ public class IsStringValid {
         }
 
         char[] chars = str.toCharArray();
-        int status = 0;
+        int count = 0;
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i] != '(' && chars[i] != ')') {
+            if (chars[i] != '(' && chars[i] != ')') {//中间混进来别的字符
                 return false;
             }
-            if (chars[i] == ')' && --status < 0) {
+            if (chars[i] == ')' && --count < 0) {
                 return false;
             }
             if (chars[i] == '(') {
-                status++;
+                count++;
             }
         }
-        return status == 0;
+        return count == 0;//遍历完之后，判断
     }
 
 }
