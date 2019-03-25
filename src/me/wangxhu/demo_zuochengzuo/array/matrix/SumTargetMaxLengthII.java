9684a3ecaa36dcf6e55a2177463dfb82075d5c6a
StormWangxhu <StormWangxhu@163.com>
2019-03-25 16:57:03 +0800

    未排序数组中累加和等于给定值的最长子数组问题，数组中值可正可负可0

--- src/me/wangxhu/demo_zuochengzuo/array/matrix/SumTargetMaxLengthII.java
+++ src/me/wangxhu/demo_zuochengzuo/array/matrix/SumTargetMaxLengthII.java
@@ -22,6 +22,7 @@ public class SumTargetMaxLengthII {
         int len = 0;
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
             if (map.containsKey(sum - k)) {
                 len = Math.max(i - map.get(sum - k), len);
             }
