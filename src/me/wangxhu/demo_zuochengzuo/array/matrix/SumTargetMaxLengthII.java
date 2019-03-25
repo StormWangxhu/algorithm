521c1776506309ad63a17cee74b5c70dc781c40d
StormWangxhu <StormWangxhu@163.com>
2019-03-25 17:08:14 +0800

    未排序数组中累加和等于给定值的最长子数组问题，数组中值可正可负可0

--- src/me/wangxhu/demo_zuochengzuo/array/matrix/SumTargetMaxLengthII.java
+++ src/me/wangxhu/demo_zuochengzuo/array/matrix/SumTargetMaxLengthII.java
@@ -8,6 +8,8 @@ import java.util.Map;
  * @Time: 2019-03-25 16:47
  * @Email: StormWangxhu@163.com
  * @Description: 未排序数组中累加和为给定值的最长子数组问题，其中数组中元素可正，可负，可0
+ * 时间复杂度：O（N）
+ * 空间复杂度：O（N）
  */
 public class SumTargetMaxLengthII {
 
@@ -17,10 +19,10 @@ public class SumTargetMaxLengthII {
             return 0;
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();//key:表示从arr中最左边开始累加过程中出现的sum值，value：表示sum值最早出现的位置。
         map.put(0, -1);//很重要，要把这个值提前存入到map中
-        int len = 0;
-        int sum = 0;
+        int len = 0;//len表示累加和为k的最长子数组长度
+        int sum = 0;//sum表示从位置0开始一直累加到到位置i的数组和
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
             if (map.containsKey(sum - k)) {
